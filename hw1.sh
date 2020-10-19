c5e1be912a44c6fc50dda8bf2fb9ec7fb8967068
chiuchiu <q302628714@163.com>
2020-10-19 23:34:01 +0800

    Delete hw1.sh

--- hw1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for i in $(seq 1 100)
-do
-    mkdir "DDM${i}"
-    text="nanoseconds since 1970-01-01 00:00:00 UTC:\n"$(date "+%s%N")
-    echo $text > "DDM${i}/time_till_now.txt"
-done
